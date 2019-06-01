53a6fed94707e3eec2517d6a5091219c45e6c93a
FranckMuller <dmitrysvetlov113@gmail.com>
2019-06-01 19:36:52 +0300

    deploy to heroku

--- src/services/fmapi-service.js
+++ src/services/fmapi-service.js
@@ -1,7 +1,7 @@
 class FmpaiService {
 
   async getAllPlayers() {
-    const data = await fetch('http://localhost:3000/data.json');
+    const data = await fetch('https://react-football-manager.herokuapp.com/data.json');
     const players = await data.json();
     return players;
   };
