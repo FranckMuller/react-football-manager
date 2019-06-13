4a5183d7963936a118817c1568f8b1e142fb2c16
FranckMuller <dmitrysvetlov113@gmail.com>
2019-06-13 17:51:57 +0300

    update 13.06

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 
 import reducer from './reducers';
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
